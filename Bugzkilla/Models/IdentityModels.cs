6a3ff6961a7fea6806d0c78b648726b55da02cd9
shadi.ganem <shadi.ganem@trustsourcing.com>
2016-10-12 15:59:34 +0300

    db name + gitinore connection string

--- Bugzkilla/Models/IdentityModels.cs
+++ Bugzkilla/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Bugzkilla.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("BugzkillaDbContext", throwIfV1Schema: false)
         {
         }
 
